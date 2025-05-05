564488424507d7ba6bd412bd1f4731b9535d2c09
Seppe Mestdagh <mestdaghseppe@gmail.com>
2025-05-05 10:58:13 +0200

    fixed cross collider

--- Bomberman/CrossCollider.cpp
+++ Bomberman/CrossCollider.cpp
@@ -9,25 +9,25 @@ bomberman::CrossCollider::CrossCollider(dae::GameObject& gameObject, CollisionTy
 	CollidersManager::GetInstance().AddCollider(*this);
 	int crossSize = 3;
 
+	//float m_Modifier = 0.75f;
+
 	float lineWidth = crossSize * TILE_SIZE * 0.8f;
 	float lineHeight = TILE_SIZE / 2;
-	float lineX = -((crossSize - 1) / 2) * TILE_SIZE * m_Modifier;
+	////float lineX = ((crossSize - 1) / 2) * TILE_SIZE * m_Modifier;
+	float lineX = crossSize * 0.1f * TILE_SIZE;
+	float lineY = (TILE_SIZE * crossSize * 0.41f);
 
-	Box horizontalLine{ lineX, (TILE_SIZE * 0.5f), lineWidth, lineHeight };
-	Box verticalLine{ (TILE_SIZE * 0.5f), lineX, lineHeight, lineWidth };
+	Box horizontalLine{ lineX, lineY, lineWidth, lineHeight};
+	Box verticalLine{ lineY, lineX, lineHeight, lineWidth };
+
+	//Box horizontalLine{ lineX, 0.0f, lineWidth, lineHeight };
+	//Box verticalLine{ lineY, lineX, lineHeight, lineWidth };
 
 	//Box horizontalLine{ -((crossSize - 1) / 2) * TILE_SIZE, 0.0f, crossSize * TILE_SIZE, TILE_SIZE};
 	//Box verticalLine{ 0.0f, -((crossSize - 1) / 2) * TILE_SIZE, TILE_SIZE, crossSize * TILE_SIZE };
 
 	m_Polygon = CreateCross(Cross(horizontalLine, verticalLine));
 	m_Axes = CalculateAxes(m_Polygon);
-}
-
-void bomberman::CrossCollider::RenderImGui()
-{
-	ImGui::Begin("Debug");
-	ImGui::SliderFloat("modifier", &m_Modifier, 0.0f, 1.0f);
-	ImGui::End();
 }
 
 polygon bomberman::CrossCollider::CreateCross(Cross cross) const
@@ -50,23 +50,38 @@ polygon bomberman::CrossCollider::CreateCross(Cross cross) const
 	//		  8*7
 
 	float a = cross.verticalX - cross.horizontalX;
-	float b = cross.horizontalWidth - a - cross.verticalWidth;
+	//float b = cross.horizontalWidth - a - cross.verticalWidth;
 	float c = cross.horizontalY - cross.verticalY;
-	float d = cross.verticalHeight - c - cross.horizontalHeight;
+	//float d = cross.verticalHeight - c - cross.horizontalHeight;
 
 	return 
 	{
-		{a,			c},
-		{a,			0.0f},
-		{a + cross.verticalWidth,		0.0f},
-		{a + cross.verticalWidth,		c},
-		{a + cross.verticalWidth + b,		c},
-		{a + cross.verticalWidth + b,		c + cross.horizontalHeight},
-		{a + cross.verticalWidth,		c + cross.horizontalHeight},
-		{a + cross.verticalWidth,		c + cross.horizontalHeight + d},
-		{a,		c + cross.horizontalHeight + d},
-		{a,		c + cross.horizontalHeight},
-		{0.0f,		c + cross.horizontalHeight},
-		{0.0f,		c},
+		{cross.horizontalX, cross.horizontalY},
+		{cross.horizontalX + a, cross.horizontalY},
+		{cross.horizontalX + a, cross.horizontalY - c},
+		{cross.horizontalX + a + cross.verticalWidth, cross.horizontalY - c},
+		{cross.horizontalX + a + cross.verticalWidth, cross.horizontalY},
+		{cross.horizontalX + cross.horizontalWidth, cross.horizontalY},
+		{cross.horizontalX + cross.horizontalWidth, cross.horizontalY + cross.horizontalHeight},
+		{cross.horizontalX + a + cross.verticalWidth, cross.horizontalY + cross.horizontalHeight},
+		{cross.horizontalX + a + cross.verticalWidth, cross.verticalY + cross.verticalHeight},
+		{cross.horizontalX + a, cross.verticalY + cross.verticalHeight},
+		{cross.horizontalX + a,  cross.horizontalY + cross.horizontalHeight},
+		{cross.horizontalX,  cross.horizontalY + cross.horizontalHeight},
 	};														 
+	//return 
+	//{
+	//	{a,			c},
+	//	{a,			0.0f},
+	//	{a + cross.verticalWidth,		0.0f},
+	//	{a + cross.verticalWidth,		c},
+	//	{a + cross.verticalWidth + b,		c},
+	//	{a + cross.verticalWidth + b,		c + cross.horizontalHeight},
+	//	{a + cross.verticalWidth,		c + cross.horizontalHeight},
+	//	{a + cross.verticalWidth,		c + cross.horizontalHeight + d},
+	//	{a,		c + cross.horizontalHeight + d},
+	//	{a,		c + cross.horizontalHeight},
+	//	{0.0f,		c + cross.horizontalHeight},
+	//	{0.0f,		c},
+	//};														 
 }
